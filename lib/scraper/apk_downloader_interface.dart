b9e3ccb4af024f0070a36f286a7c55e49ad28bb5
Aunali321 <aunvakil.aa@gmail.com>
2023-03-17 00:59:41 +0530

    feat: add param parameter

--- lib/scraper/apk_downloader_interface.dart
+++ lib/scraper/apk_downloader_interface.dart
@@ -1,7 +1,7 @@
 import 'package:revanced_manager/scraper/apk_info.dart';
 
 abstract class IApkDownloader {
-  Future<ApkInfo> getApkInfo(String packageName) {
+  Future<ApkInfo> getApkInfo(String packageName, Map<String, String> params) {
     throw UnimplementedError();
   }
 }
